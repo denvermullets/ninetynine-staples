ea60de9abc060d076ef1ecd54d9265ceef3af717
denvermullets <devmullets@gmail.com>
2022-01-02 12:26:25 -0500

    feat(api): setup api versioning, add rubocop settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :magic_cards
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :magic_cards
+    end
+  end
 end

3a659a0a87849e7dbe4942c0e9fe8f8328fce611
denvermullets <devmullets@gmail.com>
2022-08-12 09:18:35 -0400

    chore(magicCards): add index method w/types

--- app/controllers/api/v1/magic_cards_controller.rb
+++ app/controllers/api/v1/magic_cards_controller.rb
@@ -1,7 +1,16 @@
+# typed: strict
+
 module Api
   module V1
     class MagicCardsController < ApplicationController
-      # be awesome
+      extend T::Sig
+
+      sig { returns(T::Array[JSON::Ext::Generator::GeneratorMethods::Object]) }
+      def index
+        all_cards = MagicCard.where(id: 1..100)
+
+        render json: all_cards
+      end
     end
   end
 end

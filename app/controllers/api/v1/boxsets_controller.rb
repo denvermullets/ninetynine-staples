6a637886e14b50fbf96eab1391d3e2bef7370b3c
Ryan V <47340962+denvermullets@users.noreply.github.com>
2022-08-28 07:42:20 -0400

    feat(boxsets): controller actions for loading up cards in a set (#22)

--- app/controllers/api/v1/boxsets_controller.rb
+++ app/controllers/api/v1/boxsets_controller.rb
@@ -1,6 +1,24 @@
+# typed: true
+
 module Api
   module V1
     class BoxsetsController < ApplicationController
+      extend T::Sig
+
+      sig { returns(String) }
+      def index
+        boxsets = Boxset.all.order(release_date: :asc)
+
+        render json: boxsets
+      end
+
+      sig { returns(String) }
+      def show
+        boxset = Boxset.includes(magic_cards: { magic_card_color_idents: :color }).find(params[:id])
+
+        render json: boxset,
+               include: { magic_cards: { include: { magic_card_color_idents: { include: { color: { only: :name } } } } } }
+      end
     end
   end
 end

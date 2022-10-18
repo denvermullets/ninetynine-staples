32108d9abe66fb7a451c46ed87085c6e73064692
denvermullets <devmullets@gmail.com>
2022-10-18 06:29:52 -0400

    fix(ingest): allow trigger of individual sets

--- app/controllers/api/v1/ingest_controller.rb
+++ app/controllers/api/v1/ingest_controller.rb
@@ -1,15 +1,20 @@
-# typed: ignore
+require 'open-uri'
 
 module Api
   module V1
     class IngestController < ApplicationController
       def ingest_cards
         if params[:ingest]
-          puts 'hi, we are going to ingest now'
-
           IngestSets.perform_async
         elsif params[:wipe]
           CleanTables.perform_async
+        elsif params[:set]
+          source = URI.open('https://mtgjson.com/api/v5/SetList.json')
+          all_info = JSON.parse(source.read)['data']
+
+          set = all_info.filter { |single_set| single_set['code'] == params[:set] }.first
+
+          IngestSetCards.perform_async(set)
         end
       end
     end

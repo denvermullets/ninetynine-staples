1f63729a6d3d0a22c21b6b0b2295634a0bbd4372
Ryan V <47340962+denvermullets@users.noreply.github.com>
2022-10-23 13:38:07 -0400

    chore(collections): update collection order to be more efficient (#90)

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -63,7 +63,7 @@ module Api
           collection:, rarity: params[:rarity], color: params[:color], exact: params[:exact]
         )
 
-        sort_collection(player_collection)[start_range..end_range]
+        player_collection.where.not(quantity: nil || 0, foil_quantity: nil || 0).order_by_price[start_range..end_range]
       end
 
       def sort_collection(collection)

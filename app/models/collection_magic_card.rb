e1a5c19c7ba3cddd34c885d56776853978533851
denvermullets <devmullets@gmail.com>
2022-10-23 14:44:57 -0400

    chore(collections): update to handle foil price as well

--- app/models/collection_magic_card.rb
+++ app/models/collection_magic_card.rb
@@ -5,5 +5,5 @@ class CollectionMagicCard < ApplicationRecord
   scope :by_set, ->(id, set) { includes(:magic_card).where(collection_id: id, magic_card: { boxset_id: set }) }
   scope :by_id, ->(id) { includes(:magic_card, magic_card: :boxset).where(collection_id: id) }
   scope :by_boxset, ->(id) { includes(:magic_card).where(magic_card: { boxset_id: id }) }
-  scope :order_by_price, -> { includes(:magic_card).order('magic_cards.normal_price desc') }
+  scope :order_by_price, -> { includes(:magic_card).order(['magic_cards.normal_price desc', 'magic_cards.foil_price desc']) }
 end

f2ab95258f93a6d37c7834d4164e64fcdcc5b0ca
Ryan V <47340962+denvermullets@users.noreply.github.com>
2023-03-25 11:14:26 -0400

    fix(collection): account for quantity when sorting (#103)

--- app/models/collection_magic_card.rb
+++ app/models/collection_magic_card.rb
@@ -5,13 +5,19 @@ class CollectionMagicCard < ApplicationRecord
   scope :by_set, ->(id, set) { includes(:magic_card).where(collection_id: id, magic_card: { boxset_id: set }) }
   scope :by_id, ->(id) { includes(:magic_card, magic_card: :boxset).where(collection_id: id) }
   scope :by_boxset, ->(id) { includes(:magic_card).where(magic_card: { boxset_id: id }) }
-  # AI gave me this scope ;_;
+  # AI gave me this scope ;_;. sorts by whichever price is higher if quantity exists
   scope :sorted_by_combined_prices, ->(direction: 'desc') {
     direction = %w[asc desc].include?(direction.downcase) ? direction.downcase : 'desc'
     magic_cards_table = MagicCard.arel_table
+    collection_magic_cards_table = CollectionMagicCard.arel_table
     greatest_price = Arel::Nodes::NamedFunction.new(
       'GREATEST',
-      [magic_cards_table[:normal_price], magic_cards_table[:foil_price]]
+      [
+        Arel::Nodes::Case.new
+          .when(collection_magic_cards_table[:foil_quantity].eq(0))
+          .then(magic_cards_table[:normal_price])
+          .else(magic_cards_table[:foil_price])
+      ]
     )
     order_clause = "#{greatest_price.to_sql} #{direction.upcase}"
     joins(:magic_card)
